e63c88651d8878a6953918a137c807491d99a2b1
Ever VC <ever.vasquezc27@gmail.com>
2024-07-18 20:17:36 -0600

    :package: build: Dependencias y paquetes

--- Dominio/obj/Debug/net7.0-windows/Dominio.AssemblyInfo.cs
+++ Dominio/obj/Debug/net7.0-windows/Dominio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dominio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e0f278a37c4b0cd74fb016edfb51ee05df93adc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dominio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dominio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
