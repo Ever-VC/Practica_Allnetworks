804faa89df373e832c0f0008db4257c4846c56cf
Ever VC <ever.vasquezc27@gmail.com>
2024-07-21 16:21:40 -0600

    :package: build: Configuracion autogenerada de archivos dll

--- Presentacion/obj/Debug/net7.0-windows/Presentacion.AssemblyInfo.cs
+++ Presentacion/obj/Debug/net7.0-windows/Presentacion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e0f278a37c4b0cd74fb016edfb51ee05df93adc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+226afa0b8f558207ff555a341366da2568d4fe8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Soporte/obj/Debug/net7.0-windows/Soporte.AssemblyInfo.cs
+++ Soporte/obj/Debug/net7.0-windows/Soporte.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Soporte")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e0f278a37c4b0cd74fb016edfb51ee05df93adc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+226afa0b8f558207ff555a341366da2568d4fe8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Soporte")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Soporte")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
